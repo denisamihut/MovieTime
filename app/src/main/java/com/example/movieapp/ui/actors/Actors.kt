ee203d683be672d19c022b0ce2924de8a6d0c464
denisamihut <denisa.m_19@yahoo.com>
2022-07-23 15:47:07 +0300

    finished ui splashScreen

--- app/src/main/java/com/example/movieapp/ui/actors/Actors.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.ui.actors
-
-class Actors (
-    var id: Int,
-    var name: String,
-    var isSelected: Boolean
-)
---
+++ app/src/main/java/com/example/movieapp/ui/actors/Results.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.ui.actors
+
+class Actors ( //results
+//    var id: Int,
+//    var name: String,
+//    var isSelected: Boolean
+    var profile_path: String,
+    var adult: Boolean,
+    var id: Int,
+    var name: String,
+    var popularity: Number,
+    var isSelected: Boolean
+)
