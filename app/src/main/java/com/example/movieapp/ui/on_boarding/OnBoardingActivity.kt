2b6d485664aa7c13a276278732f3d071a0435a81
denisamihut <denisa.m_19@yahoo.com>
2022-08-02 14:07:22 +0300

    commit before search error

--- app/src/main/java/com/example/movieapp/ui/on_boarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/on_boarding/OnBoardingActivity.kt
@@ -6,10 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.movieapp.R
+import com.example.movieapp.SearchActivity
 import com.example.movieapp.ui.actors.ActorsActivity
+import com.example.movieapp.ui.actors.ActorsRepository
 import com.example.movieapp.ui.genres.GenresActivity
+import com.example.movieapp.ui.genres.GenresRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private val genresRepository = GenresRepository.instance
+    private val actorsRepository = ActorsRepository.instance
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
@@ -18,10 +29,11 @@ class OnBoardingActivity : AppCompatActivity() {
 
     private fun setClickListeners() {
         val genresButton = findViewById<Button>(R.id.btnGenres)
+        val actorsButton = findViewById<Button>(R.id.btnActors)
+
         genresButton.setOnClickListener {
             startActivity(Intent(this, GenresActivity::class.java))
         }
-        val actorsButton = findViewById<Button>(R.id.btnActors)
         actorsButton.setOnClickListener {
             startActivity(Intent(this, ActorsActivity::class.java))
         }
@@ -30,6 +42,23 @@ class OnBoardingActivity : AppCompatActivity() {
     companion object {
         fun open(context: Context) {
             context.startActivity(Intent(context, OnBoardingActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        verifyFilterIsSelected()
+    }
+
+    private fun verifyFilterIsSelected() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val genreCount = genresRepository.getCount()
+            val actorCount = actorsRepository.getCount()
+            withContext(Dispatchers.Main) {
+                if (genreCount > 0 && actorCount > 0) {
+                    SearchActivity.open(this@OnBoardingActivity)
+                }
+            }
         }
     }
 }
