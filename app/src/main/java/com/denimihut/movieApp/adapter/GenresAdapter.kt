8838ac391925bf74e5d411077e3682fa00044750
denisamihut <denisa.m_19@yahoo.com>
2022-08-30 06:40:00 +0300

    made genres screen functionality 90%

--- app/src/main/java/com/denimihut/movieApp/adapter/GenresAdapter.kt
+++ app/src/main/java/com/denimihut/movieApp/adapter/GenresAdapter.kt
@@ -16,6 +16,7 @@ class GenresAdapter(private val genresList: List<Genres>) :
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val parentView: ConstraintLayout = view.findViewById(R.id.clGenre)
         val genreName: TextView = view.findViewById(R.id.tvGenreName)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -45,19 +46,28 @@ class GenresAdapter(private val genresList: List<Genres>) :
 //            }
 //        )
 
-//        holder.genreName.setBackgroundColor(
+//        holder.shape1?.setBackgroundColor(
 //            when (genre.isSelected) {
-//                true -> ContextCompat.getColor(holder.parentView.context, R.color.yellow_neon)
+//                true -> ContextCompat.getColor(holder.parentView.context, R.color.blue_1)
 //                else -> ContextCompat.getColor(holder.parentView.context, R.color.yellow_neon)
 //            }
 //        )
 
         holder.genreName.setTextColor(
             when (genre.isSelected) {
-                true -> ContextCompat.getColor(holder.parentView.context, R.color.red_neon)
+                true -> ContextCompat.getColor(holder.parentView.context, R.color.grey_neon)
                 else -> ContextCompat.getColor(holder.parentView.context, R.color.black)
             }
         )
+
+//        holder.shape1?.visibility  = when (genre.isSelected) {
+//            true -> View.VISIBLE
+//            else -> View.INVISIBLE
+//        }
+//        holder.shape2?.visibility  = when (genre.isSelected) {
+//            true -> View.VISIBLE
+//            else -> View.INVISIBLE
+//        }
     }
 
     override fun getItemCount() = genresList.size
